e2a48dd85d2c4ed2919d6579aad6a3384a333862
unknown <lyacine230@gmail.com>
2023-12-07 01:46:48 +0100

    adding some improvements !

--- public/script.js
+++ public/script.js
@@ -1,3 +1,32 @@
+/*menu button js*/
+const header = document.querySelector("header");
+const menuButton = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-links");
+const exitButton = document.querySelector(".exit");
+menuButton.onclick = () => {
+  if (menuItems.classList.contains("hidden")) {
+    menuItems.classList.remove("hidden");
+    menuButton.innerHTML = "X";
+    header.style.backgroundColor = "#09151cb5";
+  } else {
+    menuItems.classList.add("hidden");
+    menuButton.innerHTML = '<svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#fff" fill-rule="evenodd" d="M18 5a1 1 0 100-2H2a1 1 0 000 2h16zm0 4a1 1 0 100-2h-8a1 1 0 100 2h8zm1 3a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 5a1 1 0 100-2h-8a1 1 0 100 2h8z"/></svg>';
+    header.style.backgroundColor = "transparent";
+  }
+};
+
+document.addEventListener("scroll", (e) => {
+  if (window.scrollY > 200) {
+    header.style.backgroundColor = "#09151cb5";
+    header.style.padding = "0px 5%";
+    menuItems.style.top = "8%";
+  } else {
+    header.style.backgroundColor = "transparent";
+    header.style.padding = "15px 5%";
+    menuItems.style.top = "11%";
+  }
+});
+
 const form = document.querySelector("form");
 let j;
 j = 0;
@@ -24,3 +53,10 @@ form.addEventListener("submit", (e) => {
     document.getElementById("sub").value = "You are already registered!";
   }
 });
+
+/*for(var c=0;menuItems.children.length-1;c++){
+    menuItems.children[c].onclick=()=>{
+        menuItems.classList.add('hidden');
+        menuButton.innerHTML='Menu';
+    }
+}*/
